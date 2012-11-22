346723b029d82a2392905435ef342a55b150966c
kiarashplusplus <kiarash@mit.edu>
2012-11-22 01:10:31 -0500

    lab5

---
+++ lab5/key.py
@@ -0,0 +1,6 @@
+# Login information for the 6.034 Automated Tester
+
+USERNAME="kiarash_MIT_EDU"
+PASSWORD="XH5N34MnmJxDcJFzPmWe"
+XMLRPC_URL="https://6.034.scripts.mit.edu:444/fall12/tester/xmlrpc/"
+
